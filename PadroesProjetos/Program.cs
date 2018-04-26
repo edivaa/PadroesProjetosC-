1586242a7f140e00ba8e2ba707d65a8a91b801f7
Edivã Goncalves Mac <edivaa@gmail.com>
2018-04-26 20:55:24 -0300

    Arquivos de configuracao projeto

---
+++ PadroesProjetos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PadroesProjetos.Classes;
+using PadroesProjetos.Classes.Objetos;
+namespace PadroesProjetos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            //Aluno aluno = new Aluno();
+            //aluno.Nome = "Paulo";
+            //aluno.Idade = 25;
+            //aluno.Dormir();
+
+
+            //Funcionario funcionario = new Funcionario();
+            //funcionario.Nome = "Gilberto Santos";
+            //funcionario.Idade = 29;
+            //funcionario.Departamento = "Imóveis";
+            //funcionario.trabalhar();
+
+
+            MeioTransporte carro = new Carro("Astra GSI");
+
+            carro.Ligar();
+            carro.Mover();
+            carro.Abastecer();
+            carro.Mover();
+
+            //MeioTransporte aviao = new Aviao();
+            //aviao.Marca = "Embrae";
+            //aviao.Modelo = "F22";
+            //aviao.Mover();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
