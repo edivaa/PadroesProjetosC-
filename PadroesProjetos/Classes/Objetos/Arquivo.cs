64adeb3f443bb09e31f4028039f6fb17f10a31dc
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-04-27 12:37:28 -0300

    Criacao de interfaces

--- PadroesProjetos/Classes/Objetos/Arquivo.cs
+++ PadroesProjetos/Classes/Objetos/Arquivo.cs
@@ -3,11 +3,35 @@ namespace PadroesProjetos.Classes.Objetos
 {
 
 
+    public interface IEditavel
+    {
+        void Editar();
+
+    }
+
+    public interface IImprimir
+    {
+        void Imprimir();
+    }
+
     public abstract class Arquivo{
 
 
         private int _tamanho;
+        public int TamArquivo
+        {
+            get { return _tamanho; }
+            set { _tamanho = value; }
+        }
         private string _descricao;
+        public string Descricao
+        {
+            get { return _descricao; }
+            set { _descricao = value; }
+
+        }
+
+
 
 
         public virtual void abrir(){
@@ -21,14 +45,33 @@ namespace PadroesProjetos.Classes.Objetos
         
     }
 
-    public class Foto : Arquivo{
-        
+    public class Foto : Arquivo, IImprimir, IEditavel
+    {
+        public void Editar()
+        {
+            //
+        }
+
+        public void Imprimir()
+        {
+            //
+        }
     }
 
-    public class Documento : Arquivo
+    public class Documento : Arquivo, IImprimir, IEditavel
     {
         public Documento()
         {
+        }
+
+        public void Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Imprimir()
+        {
+            throw new NotImplementedException();
         }
     }
 }
