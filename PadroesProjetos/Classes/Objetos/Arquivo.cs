013d125387585f719c95db805001479c1efff302
Edivã Goncalves Mac <edivaa@gmail.com>
2018-04-27 06:13:09 -0300

    Ïnicando cricacao de interfaces

---
+++ PadroesProjetos/Classes/Objetos/Arquivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace PadroesProjetos.Classes.Objetos
+{
+
+
+    public abstract class Arquivo{
+
+
+        private int _tamanho;
+        private string _descricao;
+
+
+        public virtual void abrir(){
+
+            Console.WriteLine("Abrindo arquivo");
+        }
+
+    }
+
+    public class MP3: Arquivo{
+        
+    }
+
+    public class Foto : Arquivo{
+        
+    }
+
+    public class Documento : Arquivo
+    {
+        public Documento()
+        {
+        }
+    }
+}
