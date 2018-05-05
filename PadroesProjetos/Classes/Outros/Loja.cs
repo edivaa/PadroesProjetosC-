7634ed213f291f09848391570d9f086103c1952e
Edivã Goncalves Mac <edivaa@gmail.com>
2018-05-05 07:41:21 -0300

    Arquivos de configuracao entity

--- PadroesProjetos/Classes/Outros/Loja.cs
+++ PadroesProjetos/Classes/Outros/Loja.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PadroesProjetos.Classes.Outros
 {
+    [Table("Loja")]
     public class Loja
     {
+
         public Loja()
         {
+            Produto = new List<Produto>();
         }
+
+
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+
+        public virtual Loja Loja { get; set; }
+
+        public virtual List<Produto> Produtos { get; set; }
+    }
     }
 }
