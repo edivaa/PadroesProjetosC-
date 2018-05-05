eebd89268f2aa3fd10361dd2c73e2a14ca3c2eed
Edivã Goncalves Mac <edivaa@gmail.com>
2018-05-05 07:17:45 -0300

    Criacao da classe Produto

--- PadroesProjetos/Classes/Objetos/Produto.cs
+++ PadroesProjetos/Classes/Objetos/Produto.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PadroesProjetos.Classes.Objetos
 {
-    public class Prooduto
+    [Table("Produto")]
+    public class Produto
     {
+
+       
+        [Key]//DataAnnotations
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)] //Defini tamanho maximo do campo no banco
         public string Nome { get; set; }
+        [MaxLength(2000)] 
         public string Descricao { get; set; }
+
+        [Range(999999999999.999, 999999999999.999)]
+        [Required]
         public decimal Valor { get; set; }
-    }
+        [ForeignKey("Loja")]
+        public int LojaId { get; set; }
+
+
 }
