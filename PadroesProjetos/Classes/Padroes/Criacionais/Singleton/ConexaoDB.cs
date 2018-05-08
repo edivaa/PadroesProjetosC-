4ed9e2104cb7a4834b87c8abbdc7e04c05e2cb6d
Edivã Goncalves Mac <edivaa@gmail.com>
2018-05-08 20:29:39 -0300

    Criacao da classe singleton

--- PadroesProjetos/Classes/Padroes/Criacionais/Singleton/ConexaoDB.cs
+++ PadroesProjetos/Classes/Padroes/Criacionais/Singleton/ConexaoDB.cs
@@ -3,8 +3,30 @@ namespace PadroesProjetos.Classes.Padroes.Criacionais.Singleton
 {
     public class ConexaoDB
     {
-        public ConexaoDB()
-        {
-        }
+		//guarda internamente a intancia
+		private static ConexaoDB _instance;
+
+		private ConexaoDB(){}
+
+        //método que retorna singleton
+		public static ConexaoDB Instance()
+		{
+
+			//garantir que sempre uma instancia exista
+			if (_instance == null)
+			{
+				_instance = new ConexaoDB();
+			}
+
+			return _instance;
+
+		}
+
+
+		public string stringConexao{ get; set; }
+		public void Open(){
+			Console.WriteLine("Abrindo Conexão "+stringConexao); 
+		}
+
     }
 }
